fb044f4c8c6a1328681508aa9800d071e37f5e74
Luke Short <luke.short@metergroup.com>
2019-02-23 13:45:28 -0800

    bg color

--- src/entry.js
+++ src/entry.js
@@ -7,10 +7,9 @@
  * 
  */
 
-// import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three';
+// import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three'; // other import method easier?
 var THREE = require('three');
 require('three-fly-controls')(THREE);
-// import { FlyControls } from 'three-fly-controls';
 import SeedScene from './objects/Scene.js';
 
 const scene = new THREE.Scene();
@@ -25,17 +24,10 @@ scene.add(seedScene);
 camera.position.set(6,3,-10);
 camera.lookAt(new THREE.Vector3(0,0,0));
 var controls = new THREE.FlyControls( camera , renderer.domElement);
-// controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-// controls.dampingFactor = 0.25;
-// controls.screenSpacePanning = false;
-// controls.minDistance = 100;
-// controls.maxDistance = 500;
-// controls.maxPolarAngle = Math.PI / 2;
 
 // renderer
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(0x7ec0ee, 1);
+renderer.setClearColor(0x041320, 1);
 
 // render loop
 const onAnimationFrameHandler = (timeStamp) => {
@@ -59,49 +51,4 @@ window.addEventListener('resize', windowResizeHanlder);
 // dom
 document.body.style.margin = 0;
 document.body.appendChild( renderer.domElement );
-// var isMouseDown = false;
-// var onMouseDownPosition = {};
-// document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-// document.body.addEventListener("mousedown", function(event) {
-//   isMouseDown = true
-//   onMouseDownPosition.x = event.clientX;
-//   onMouseDownPosition.y = event.clientY;
-// }, false);
-// document.body.addEventListener("mouseup", function(event) {
-//   isMouseDown = false
-// }, false);
 
-// function render() {
-//   renderer.render(scene, camera);
-// }
-// function onDocumentMouseMove( event ) { // this gets hoisted, move this to a separate file to make cleaner
-//   console.log('event received: ', event);
-//   event.preventDefault();
-
-//   if ( isMouseDown ) {
-//       theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 )
-//               + onMouseDownTheta;
-//       phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 )
-//             + onMouseDownPhi;
-//       phi = Math.min( 180, Math.max( 0, phi ) );
-
-//       camera.position.x = radious * Math.sin( theta * Math.PI / 360 )
-//                           * Math.cos( phi * Math.PI / 360 );
-//       camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
-//       camera.position.z = radious * Math.cos( theta * Math.PI / 360 )
-//                           * Math.cos( phi * Math.PI / 360 );
-//       camera.updateMatrix();
-//   }
-
-//   mouse3D = projector.unprojectVector(
-//       new THREE.Vector3(
-//           ( event.clientX / renderer.domElement.width ) * 2 - 1,
-//           - ( event.clientY / renderer.domElement.height ) * 2 + 1,
-//           0.5
-//       ),
-//       camera
-//   );
-//   ray.direction = mouse3D.subSelf( camera.position ).normalize();
-//   interact();
-//   render();
-// }
